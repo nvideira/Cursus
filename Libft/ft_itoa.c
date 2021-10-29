1a900f386da7d2c08e399df67a0de79dfe12b706
nvideira <87093430+nvideira@users.noreply.github.com>
2021-10-29 18:41:57 +0100

    Add files via upload

---
+++ Libft/ft_itoa.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nvideira <nvideira@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/29 17:44:00 by nvideira          #+#    #+#             */
+/*   Updated: 2021/10/29 18:40:45 by nvideira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	nblen(int n)
+{
+	int	len;
+
+	len = 0;
+	if (n < 0)
+	{
+		len = 1;
+		n = -n;
+	}
+	while (n > 0)
+	{
+		n /= 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*s;
+	int	len;
+	
+	len = nblen(n);
+	s = malloc((len + 1) * sizeof(char));
+	if (!s)
+		return (NULL);
+	s[len + 1] = '\0';
+	if (n < 0)
+	{
+		s[0] = '-';
+		n = -n
+	}
+	if (n = -2147483648)
+	{
+		s[0] = '-';
+		
+	}
+	else if (n = 0)
+		s[0] = 0;
+	else
+	{
+		while (n > 0)
+		{
+			s[len] = n % 10;
+			n /= 10;
+			len--;
+		}
+	}
+	return (s);
+	
+}
--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -6,34 +6,73 @@
 /*   By: nvideira <nvideira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/28 17:03:49 by nvideira          #+#    #+#             */
-/*   Updated: 2021/10/28 17:49:14 by nvideira         ###   ########.fr       */
+/*   Updated: 2021/10/29 17:42:16 by nvideira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	**ft_split(char const *s, char c)
+static int	ft_space(char s, char c)
 {
-	unsigned int	i;
-	unsigned int	j;
-	size_t			ni;
-	char			**new;
+	if (s == c || s == '\0')
+		return (1);
+	else
+		return (0);
+}
+
+static int	ft_wordcount(const char *str, char c)
+{
+	int		words;
+	size_t	i;
+
+	i = 0;
+	words = 0;
+	while (str[i])
+	{
+		if (ft_space(str[i], c) == 0 && ft_space(str[i + 1], c) == 1)
+			words++;
+		i++;
+	}
+	return (words);
+}
+
+static void	split_it(char const *s, char c, int st, char **ns)
+{
+	int	i;
+	int	j;
 
 	i = 0;
 	j = 0;
-	ni = 0;
-	while (*s)
+	while (s[i])
 	{
-		if (s[i] != c)
-			i++;
-		if (s[i] == c)
+		while (s[i] != '\0' && s[i] == c)
 		{
-			*new[ni] = *ft_substr(s, j, i);
-			j = i;
 			i++;
-			ni++;
+			st++;
 		}
+		if (s[i] == '\0')
+			break ;
+		if (ft_space(s[i], c) == 0 && ft_space(s[i + 1], c) == 1)
+		{
+			ns[j++] = ft_substr(s, st, (i - st) + 1);
+			st = i + 1;
+		}
+		i++;
 	}
-	*new[ni] = (char )ft_memset(*new[ni], '\0', 1);
-	return (new);
+	ns[j] = NULL;
+}
+
+char	**ft_split(const char *s, char c)
+{
+	char	**ns;
+	size_t	st;
+
+	st = 0;
+	if (!s)
+		return (NULL);
+	ns = malloc(sizeof(char *) * (ft_wordcount(s, c)));
+	if (!ns)
+		return (NULL);
+	split_it(s, c, st, ns);
+	return (ns);
 }
