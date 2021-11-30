11df9e0591ae528e2aab812752bbc5cb919475a6
Nuno Videira <nvideira@c1r19s6.42lisboa.com>
2021-11-30 17:55:49 +0000

    gnl fully functional except bonus

--- get_next_line/get_next_line_bonus.h
+++ get_next_line/get_next_line_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nvideira <nvideira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 20:12:57 by nvideira          #+#    #+#             */
-/*   Updated: 2021/11/30 17:39:56 by nvideira         ###   ########.fr       */
+/*   Updated: 2021/11/30 17:49:41 by nvideira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 # include <unistd.h>
 # include <stdlib.h>
 # include <sys/stat.h>
---
+++ get_next_line/get_next_line_utils_bonus.c
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nvideira <nvideira@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/01 20:17:01 by nvideira          #+#    #+#             */
+/*   Updated: 2021/11/30 16:20:17 by nvideira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strljoin(char const *s1, char const *s2, unsigned int len)
+{
+	char			*ns;
+	unsigned int	i;
+	unsigned int	j;
+
+	if (!s2)
+		return (NULL);
+	if (!s1)
+		return (ft_substr(s2, 0, len));
+	ns = malloc((ft_strlen(s1) + len) * sizeof(char) + 1);
+	if (!ns)
+		return (NULL);
+	i = -1;
+	while (s1[++i])
+		ns[i] = s1[i];
+	j = -1;
+	while (s2[++j] && j < len)
+		ns[i + j] = s2[j];
+	ns[i + j] = '\0';
+	return (ns);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*p;
+	int		i;
+
+	if (!s1)
+		return (NULL);
+	p = malloc(ft_strlen(s1) + 1);
+	if (!p)
+		return (NULL);
+	i = -1;
+	while (s1[++i])
+		p[i] = s1[i];
+	p[i] = '\0';
+	return (p);
+}
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	if (s)
+	{
+		while (*s != (char )c && *s != '\0')
+			s++;
+		if (*s == (char )c)
+			return ((char *)s);
+		else
+			return (NULL);
+	}
+	return (NULL);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char			*sub;
+	unsigned int	i;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	if (!len || ft_strlen(s) < start)
+		return (ft_strdup(""));
+	if (len > ft_strlen(s))
+		len = ft_strlen(s) - (size_t)start;
+	sub = malloc((len + 1) * sizeof(char));
+	if (!sub)
+		return (NULL);
+	while (s[start + i] != '\0' && i < len)
+	{
+		sub[i] = s[start + i];
+		i++;
+	}
+	sub[i] = '\0';
+	return (sub);
+}
