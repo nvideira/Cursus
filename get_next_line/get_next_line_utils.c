a1ec572ed904bd7484d54abb529cd3fdc2672851
Nuno Videira <nvideira@c1r19s4.42lisboa.com>
2021-12-01 00:51:50 +0000

    bonus fixed

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 char	*ft_strljoin(char const *s1, char const *s2, unsigned int len)
 {
